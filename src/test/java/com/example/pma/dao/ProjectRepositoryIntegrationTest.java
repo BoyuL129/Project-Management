feaa8fd0f0f09738159f470221b311881f398083
Boyu Liu <boyuliu9@ad.unc.edu>
2024-03-21 01:33:02 -0500

    environment variables

--- src/test/java/com/example/pma/dao/ProjectRepositoryIntegrationTest.java
+++ src/test/java/com/example/pma/dao/ProjectRepositoryIntegrationTest.java
@@ -1,4 +1,4 @@
-package dao;
+package com.example.pma.dao;
 
 import com.example.pma.ProjectManagementApplication;
 import com.example.pma.dao.ProjectRepository;
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
@@ -14,9 +15,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@ContextConfiguration(classes = ProjectManagementApplication.class)
+@SpringBootTest
 @RunWith(SpringRunner.class)
-@DataJpaTest
 @SqlGroup({@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = {"classpath:schema.sql", "classpath:data.sql"}),
         })
 public class ProjectRepositoryIntegrationTest {
